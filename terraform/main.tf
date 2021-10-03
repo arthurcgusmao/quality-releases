ce16efc981fae729dddc2abafc10d77e32c51f72
Arthur Colombini Gusmão <arthurcgusmao@users.noreply.github.com>
2021-10-03 14:24:33 +0200

    Terraform: move auth credentials outside .tfvars

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,4 @@
 provider "azurerm" {
-  tenant_id       = "${var.tenant_id}"
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
   features {}
 }
 terraform {
